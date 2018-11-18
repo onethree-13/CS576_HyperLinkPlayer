7decb4e6676878e19e152a03ca2897fa41520507
onethree-13 <onethreefred@gmail.com>
2018-11-18 01:12:56 -0800

    media player done

--- src/main/java/CS576/utils/timeControl.java
+++ src/main/java/CS576/utils/timeControl.java
@@ -1,23 +1,21 @@
 package CS576.utils;
 
-import javax.swing.*;
 import java.awt.Dimension;
-import java.awt.Toolkit;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.event.*;
-import java.awt.*;
-import java.lang.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Timer;
-import java.util.TimerTask;;
+import java.util.TimerTask;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;;
 
 public class timeControl extends JPanel {
-	private static String start = new String("Start");
-	private static String stop = new String("Stop ");
-	private static int frameRate = 30;
+	private final int FRAME_RATE = 30;
+	private final int TOTAL_FRAME = 9000;
 	private int frame = 0;
-	private static int totalFrames = 9000;
-	private JButton control = new JButton(start);
+	private JButton control = new JButton("start");
 	private JButton prevTenFrame = new JButton("prev 10f");
 	private JButton nextTenFrame = new JButton("next 10f");
 	private JTextField frameNum = new JTextField(6);
@@ -26,60 +24,75 @@ public class timeControl extends JPanel {
 	private JLabel frameDisplay = new JLabel("frame: 0001");
 	private Timer timer;
 
-	public timeControl(rgbPlayer player) {
-		setPreferredSize(new Dimension(800, 80));
+	private rgbPlayer rgb;
+	private wavPlayer wav;
 
+	private void play() {
+		wav.play();
+		timer = new Timer();
+		timer.schedule(new TimerTask() {
+			@Override
+			public void run() {
+				frame++;
+				frame %= TOTAL_FRAME;
+				frameDisplay.setText("frame: " + String.format("%04d", frame + 1));
+				rgb.play(frame);
+			}
+		}, (int) (1000 / FRAME_RATE), (int) (1000 / FRAME_RATE));
+	}
+
+	private void stop() {
+		timer.cancel();
+		wav.stop();
+	}
+
+	private void setFrame(int frame) {
+		rgb.play(frame);
+		wav.set(frame * 1000000 / FRAME_RATE);
+	}
+
+	public timeControl(rgbPlayer rgb, wavPlayer wav) {
+		setPreferredSize(new Dimension(800, 80));
+		this.wav = wav;
+		this.rgb = rgb;
 		control.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				startOrStop = !startOrStop;
 				if (startOrStop) {
-					control.setText(stop);
-					timer = new Timer();
-					timer.schedule(new TimerTask() {
-						@Override
-						public void run() {
-							frame++;
-							frame %= totalFrames;
-							frameDisplay.setText("frame: " + String.format("%04d", frame + 1));
-							player.load(frame);
-							player.repaint();
-						}
-					}, (int) (1000 / frameRate), (int) (1000 / frameRate));
+					control.setText("stop");
+					play();
 				} else {
-					control.setText(start);
-					timer.cancel();
+					control.setText("start");
+					stop();
 				}
 			}
 		});
 		prevTenFrame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				frame += totalFrames - 10;
-				frame %= totalFrames;
+				frame += TOTAL_FRAME - 10;
+				frame %= TOTAL_FRAME;
 				frameDisplay.setText("frame: " + String.format("%04d", frame + 1));
-				player.load(frame);
-				player.repaint();
+				setFrame(frame);
 
 			}
 		});
 		nextTenFrame.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				frame += 10;
-				frame %= totalFrames;
+				frame %= TOTAL_FRAME;
 				frameDisplay.setText("frame: " + String.format("%04d", frame + 1));
-				player.load(frame);
-				player.repaint();
+				setFrame(frame);
 			}
 		});
 		goTo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				try {
 					int f = Integer.parseInt(frameNum.getText());
-					if (f <= 0 || f > totalFrames)
+					if (f <= 0 || f > TOTAL_FRAME)
 						System.out.println("error: Out of range.");
 					frame = f;
 					frameDisplay.setText("frame: " + String.format("%04d", frame));
-					player.load(frame);
-					player.repaint();
+					setFrame(frame);
 				} catch (NumberFormatException err) {
 					System.out.println("error: Not a number.");
 				}
